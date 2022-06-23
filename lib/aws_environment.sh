203527ce7b181cead6c764f932e2908162fd53af
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2022-06-23 23:15:17 +0900

    環境変数の取る場所を修正

--- lib/aws_environment.sh
+++ lib/aws_environment.sh
@@ -2,10 +2,10 @@
 
 echo $1
 echo $2
-credentials=$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
-export AWS_ACCESS_KEY_ID=$(echo $credentials | jq -r .AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo $credentials | jq -r .SecretAccessKey)
-export AWS_SESSION_TOKEN=$(echo $credentials | jq -r .Token)
+#credentials=$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
+export AWS_ACCESS_KEY_ID=$(echo $2 | jq -r .Records[0].awsKeys.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $2 | jq -r .Records[0].awsKeys.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_SESSION_TOKEN)
 echo ${AWS_ACCESS_KEY_ID}
 
 /var/lang/bin/python3.9 /var/runtime/bootstrap.py $1 $2
