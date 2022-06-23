c5308fbdf21d9b2bb92935ac2de5d43799375eb2
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2022-06-23 23:37:30 +0900

    環境変数の取る場所を修正

--- lib/aws_environment.sh
+++ lib/aws_environment.sh
@@ -3,8 +3,8 @@
 echo $1
 echo $2
 #credentials=$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
-export AWS_ACCESS_KEY_ID=$(echo $2 | jq -r .Records[0].awsKeys.AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo $2 | jq -r .Records[0].awsKeys.SecretAccessKey)
+export AWS_ACCESS_KEY_ID=$(echo $2 | jq -r .Records[0].awsKeys.AWS_ACCESS_KEY_ID)
+export AWS_SECRET_ACCESS_KEY=$(echo $2 | jq -r .Records[0].awsKeys.AWS_SECRET_ACCESS_KEY)
 export AWS_SESSION_TOKEN=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_SESSION_TOKEN)
 echo ${AWS_ACCESS_KEY_ID}
 
