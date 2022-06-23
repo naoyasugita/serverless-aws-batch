153256a3fb94a74a894a396a92b7fc196e16d3d4
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2022-06-24 01:45:27 +0900

    Lambdaの環境変数が渡っていない

--- lib/aws_environment.sh
+++ lib/aws_environment.sh
@@ -6,8 +6,16 @@ echo $2
 export AWS_ACCESS_KEY_ID=$(echo $2 | jq -r .Records[0].awsKeys.AWS_ACCESS_KEY_ID)
 export AWS_SECRET_ACCESS_KEY=$(echo $2 | jq -r .Records[0].awsKeys.AWS_SECRET_ACCESS_KEY)
 export AWS_SESSION_TOKEN=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_SESSION_TOKEN)
-export _HANDLER=$(echo $1)
 export AWS_LAMBDA_RUNTIME_API=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_LAMBDA_RUNTIME_API)
+export LAMBDA_RUNTIME_DIR=$(echo $credentials | jq -r .Records[0].awsKeys.LAMBDA_RUNTIME_DIR)
+export _HANDLER=$(echo $credentials | jq -r .Records[0].awsKeys._HANDLER)
+export _X_AMZN_TRACE_ID=$(echo $credentials | jq -r .Records[0].awsKeys._X_AMZN_TRACE_ID)
+export AWS_REGION=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_REGION)
+export AWS_LAMBDA_FUNCTION_NAME=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_LAMBDA_FUNCTION_NAME)
+export AWS_LAMBDA_FUNCTION_MEMORY_SIZE=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_LAMBDA_FUNCTION_MEMORY_SIZE)
+export AWS_LAMBDA_FUNCTION_VERSION=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_LAMBDA_FUNCTION_VERSION)
+export AWS_LAMBDA_INITIALIZATION_TYPE=$(echo $credentials | jq -r .Records[0].awsKeys.AWS_LAMBDA_INITIALIZATION_TYPE)
+export LAMBDA_TASK_ROOT=$(echo $credentials | jq -r .Records[0].awsKeys.LAMBDA_TASK_ROOT)
 echo ${AWS_ACCESS_KEY_ID}
 echo "$(</var/runtime/bootstrap.py)"
 
